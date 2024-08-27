5a99552982192de84fdc9b3cd96694b2303357a4
Kohei Dunnet <kdunnet1@my.bcit.ca>
2024-08-26 17:34:17 -0700

    no message

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+document.getElementById('PCB').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    document.getElementById('carouselPCB').style.display = 'block';
+});
+
+document.getElementById('LCC').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    document.getElementById('carouselLCC').style.display = 'block';
+});
+
+document.getElementById('Smokeshow').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    document.getElementById('carouselPCB').style.display = 'block';
+});
